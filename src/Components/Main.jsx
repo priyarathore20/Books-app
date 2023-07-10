a773742ddf5806fdb11bb0a92b79ac35d9f929ea
priya rathore <rathorepriya1705@gmail.com>
2023-07-10 11:44:29 +0530

    initial commit

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {HiSearch} from "react-icons/hi"
 import Cards from './Cards'
+import axios from 'axios'
 
 const Main = () => {
+  const [search, setSearch] = useState("")
   return (
     <>
     <div className='header'>
@@ -12,13 +14,20 @@ const Main = () => {
       <div className="row2">
         <h2>Find your book :</h2>
         <div className="search">
-        <input type="text" placeholder='Enter name' />
+        <input value={search} onChange={e=> setSearch(e.target.value)} type="text" placeholder='Enter name' />
         <button className="search-btn"><HiSearch className='search-btnn'/></button>
         </div>
-        <img src="https://www.pikpng.com/pngl/m/352-3525670_click-here-for-flyer-kids-reading-reading-a.png" alt="#" />
+        <img src="https://assets-global.website-files.com/60e615980cab093e6f2db3c3/60f1c6979e1b269892f46f41_GettyImages-1206750602.jpeg" alt="#" />
       </div>
           </div>
           <div className="container">
+        <Cards/>
+        <Cards/>
+        <Cards/>
+        <Cards/>
+        <Cards/>
+        <Cards/>
+        <Cards/>
         <Cards/>
       </div>
     </>

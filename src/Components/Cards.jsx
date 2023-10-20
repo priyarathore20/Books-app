47df1f996b317a099114d8add521f95462fd1bab
Priya Rathore <rathorepriya1705@gmail.com>
2023-10-20 12:26:47 +0530

    correct css

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,28 +1,35 @@
-import React, { useState } from 'react';
-import Modal from './Modal';
+import React, { useState } from "react";
+import Modal from "./Modal";
 
 const Cards = ({ Book }) => {
-  const [show,setShow]=useState(false);
-  const [bookItem,setItem]=useState();
-  console.log(Book)
+  const [show, setShow] = useState(false);
+  const [bookItem, setItem] = useState();
+  console.log(Book);
   return (
     <>
       {Book.map((item, key) => {
-        key= item.id
-        let title = item.volumeInfo?.title
-        let thumbnail = item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.thumbnail;
-        let price = item.saleInfo.saleability === "FOR_SALE" ? item.saleInfo.listPrice.amount : item.saleInfo.saleability 
+        key = item.id;
+        let title = item.volumeInfo?.title;
+        let thumbnail =
+          item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.thumbnail;
+        let price =
+          item.saleInfo.saleability === "FOR_SALE"
+            ? item.saleInfo.listPrice.amount
+            : item.saleInfo.saleability;
         return (
-          <div className="card" onClick={()=>{setShow(true);setItem(item)}}>
-            <img
-              src={thumbnail}
-              alt="#"
-            />
+          <div
+            className="card"
+            onClick={() => {
+              setShow(true);
+              setItem(item);
+            }}
+          >
+            <img src={thumbnail} alt={thumbnail ? "#" : "No image available"} />
             <div>
               <h3 className="title">{title}</h3>
               <p className="amount">&#8377; {price}</p>
             </div>
-            <Modal show={show} item={bookItem} onClose={()=>setShow(false)}/>
+            <Modal show={show} item={bookItem} onClose={() => setShow(false)} />
           </div>
         );
       })}
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,40 +1,52 @@
-import React, { useState } from 'react'
-import {HiSearch} from "react-icons/hi"
-import Cards from './Cards'
-import axios from 'axios'
+import React, { useState } from "react";
+import { HiSearch } from "react-icons/hi";
+import Cards from "./Cards";
+import axios from "axios";
 
 const Main = () => {
-  const [search, setSearch] = useState("")
-  const [bookData, setBookData] = useState([])
- 
- const handleSearch = async() => {
-try {
-  const res = await axios.get(' https://www.googleapis.com/books/v1/volumes?q=' +search+ '&key=AIzaSyBTA3n1X3cfcXMDWulL8pWLKIoVhpoL3Og'+'&maxResults=40')
-  setBookData(res.data.items)
-} catch (error) {
-  console.log("error")
-}  
- }
+  const [search, setSearch] = useState("");
+  const [bookData, setBookData] = useState([]);
+
+  const handleSearch = async () => {
+    try {
+      const res = await axios.get(
+        " https://www.googleapis.com/books/v1/volumes?q=" +
+          search +
+          "&key=AIzaSyBTA3n1X3cfcXMDWulL8pWLKIoVhpoL3Og" +
+          "&maxResults=40"
+      );
+      setBookData(res.data.items);
+    } catch (error) {
+      console.log("error");
+    }
+  };
   return (
     <>
-    <div className='header'>
-      <div className="row1">
-        <h1>A Room Without Book Is Like <br /> A Body Without Soul.</h1>
-      </div>
-      <div className="row2">
-        <h2>Find your book :</h2>
-        <div className="search">
-        <input value={search} onChange={e=> setSearch(e.target.value)} type="text" placeholder='Enter name' />
-        <button className="search-btn" onClick={handleSearch}><HiSearch className='search-btnn'/></button>
+      <div className="header">
+        <div className="row1">
+          <h1 className="head-1">
+            " The more that you read, the more things you will know. The more
+            that you learn, the more places you'll go."
+          </h1>
         </div>
-        <img src="https://assets-global.website-files.com/60e615980cab093e6f2db3c3/60f1c6979e1b269892f46f41_GettyImages-1206750602.jpeg" alt="#" />
+        <div className="row2">
+          <h2>Find your book :</h2>
+          <div className="search">
+            <input
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              type="text"
+              placeholder="Enter name"
+            />
+            <button className="search-btn" onClick={handleSearch}>
+              <HiSearch className="search-btnn" />
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
-    <div className="container">
-        {<Cards Book={bookData}/>}
-    </div>
+      <div className="container">{<Cards Book={bookData} />}</div>
     </>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
